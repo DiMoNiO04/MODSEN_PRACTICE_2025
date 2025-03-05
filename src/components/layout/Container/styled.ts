226ffb9f35927032dd945a0b6c6780c56e1a8e89
Dmitry Razumov <dima.razumov.04.work@gmail.com>
2025-03-05 23:14:41 +0300

    feat: add adaptive for dashboard

--- src/components/layout/Container/styled.ts
+++ src/components/layout/Container/styled.ts
@@ -4,6 +4,10 @@ const ContainerBlock = styled.div`
   max-width: 100%;
   padding: 0 var(--padding-l);
   margin: var(--margin-auto);
+
+  @media (max-width: 1024px) {
+    padding: 0;
+  }
 `;
 
 export { ContainerBlock };
--- src/components/layout/Dashboard/styled.ts
+++ src/components/layout/Dashboard/styled.ts
@@ -5,6 +5,21 @@ const DashboardContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: var(--gap-xl);
+
+  @media (max-width: 1600px) {
+    grid-template-columns: repeat(3, 1fr);
+    padding: var(--padding-xx);
+  }
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+    padding: var(--padding-l);
+  }
+
+  @media (max-width: 767px) {
+    grid-template-columns: repeat(1, 1fr);
+    padding: 0 0 var(--padding-xxl);
+  }
 `;
 
 export { DashboardContainer };
